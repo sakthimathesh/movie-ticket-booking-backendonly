757487905d9c99032a1d02eadda1c3528a819b00
sakthimathesh <sakthimathesh2101@gmail.com>
2026-09-17 07:30:08 -0700

    Configure PostgreSQL for deployment

--- config/settings.py
+++ config/settings.py
@@ -83,16 +83,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'movie_ticket',
-        'USER': 'root',
-        'PASSWORD': 'SHIYADILEEP2114',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'movie_ticket',
+#         'USER': 'root',
+#         'PASSWORD': 'SHIYADILEEP2114',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 
 
 # Password validation
